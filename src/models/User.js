a48a33a0ac525556e5832aa65df6bc33f25f7d1b
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2019-09-14 18:49:06 -0300

    Finalizando todos os dados de User

--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,46 @@ const UserSchema = new mongoose.Schema({
     tipo: { type: String, required: true },
     cpf: { type: String, required: false },
     cnpj: { type: String, required: false },
-    
+    sexo: String,
+    raca: String,
+    nacionalidade: String,
+    deficiencia: [String],
+    trabalho: String,
+    renda: Number,
+    dependentes: Number,
+    pictureFilename: String,
+    rg: String,
+    orgaoExpedidor: String,
+    dataExpedicao: Date,
+    pisPasep: String,
+    nomePai: String,
+    nomeMae: String,
+    dataNascimento: Date,
+    localNascimento: String,
+    estadoCivil: String,
+    telefone: String,
+    email: String,
+    endereço: {
+        cep: String,
+        numero: Number,
+        complemento: String
+    },
+    planoDeSaude: String,
+    numeroCartaoPlano: String,
+    vencimentoCartaoPlano: Date,
+    dadosDrogas: {
+        cigarro: Boolean,
+        alcool: Boolean,
+        drogasIlicitas: [String],
+        remedios: [String]
+    },
+    dadosAlergias: {
+        medicamentos: String,
+        alimentos: String,
+        equipamento: String
+    },
+    tipoSanguineo: String,
+    bio: String
 });
 
 mongoose.model('User', UserSchema);
