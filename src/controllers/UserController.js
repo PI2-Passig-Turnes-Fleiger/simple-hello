209739dac4b836c3bdc92b53c17adbf96ee5f646
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2019-10-17 10:39:00 -0300

    Mudando status de respostas falhas para 400

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,11 +6,11 @@ module.exports = {
     async store(req, res) {
         const { nome, sobrenome, email, cpf, senha, confirmar_senha } = req.body;
         if(!nome || !sobrenome || !email || !cpf || !senha || !confirmar_senha)
-            return res.status(403).send('Campos incompletos!');
+            return res.status(400).send('Campos incompletos!');
         if(senha != confirmar_senha)
-            return res.status(403).send('Senhas não conferem!');
+            return res.status(400).send('Senhas não conferem!');
         if(await User.findOne({ email }))
-            return res.status(403).send('uje');
+            return res.status(400).send('uje');
         
         const user = await User.create({ nome, sobrenome, senha, tipo: 'pf', cpf, email })
         
