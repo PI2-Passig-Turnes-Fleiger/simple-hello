1c367097f68a211d289b70ab636de5a4421ef7ee
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2019-10-27 14:11:00 -0300

    Adicionando rotas para autenticação no backend

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
+require('dotenv-safe').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const requireDir = require('require-dir');
 
-const { usuario, senha } = require('./src/banco.json');
-
 const app = express();
-
 
 mongoose.connect(
     `mongodb://localhost:27017/simpleHello`,
@@ -20,7 +18,8 @@ requireDir('./src/models');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // Rotas recebidas do arquivo routes.js
 app.use('/', require('./src/routes'));
 
-app.listen(1234);
+app.listen(3003);
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const User = mongoose.model('User');
 
 module.exports = {
-    async store(req, res) {
+    async store(req, res){
         const { nome, sobrenome, email, cpf, senha, confirmar_senha } = req.body;
         if(!nome || !sobrenome || !email || !cpf || !senha || !confirmar_senha)
             return res.status(400).send('Campos incompletos!');
@@ -14,6 +15,35 @@ module.exports = {
         
         const user = await User.create({ nome, sobrenome, senha, tipo: 'pf', cpf, email })
         
-        return res.json(user);
+        return res.status(201).json(user);
+    },
+
+    async login(req, res){
+        const { email, senha } = req.body;
+
+        const user = await User.findOne({ email });
+        if(!user || user.senha !== senha)
+            res.status(500).send('Login inválido!');
+        else{
+            const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: 3000 });
+            res.json({ auth: true, token });
+        }
+    },
+
+    logout(req, res){
+        res.json({ auth: false, token: null});
+    },
+
+    verifyJWT(req, res, next){
+        const token = req.headers['access-token'];
+
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+            if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+            req.userId = decoded._id;
+            next();
+        });
     }
 };
